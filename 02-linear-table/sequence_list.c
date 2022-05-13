61e5ff14ea426ae528a9e2fcffe5e1892f584d9f
AdianGg <admin@e-wolf.top>
2022-05-13 22:00:39 +0800

    Update sequence_list.c

--- 02-linear-table/sequence_list.c
+++ 02-linear-table/sequence_list.c
@@ -8,7 +8,7 @@ void InitList(sequence_list *sequenceList,int size){
 	// init the sequence_list
 	sequenceList->size = size;
 	sequenceList->length = 0;
-	sequenceList->head = (Element*)malloc(sizeof(Element) * sequenceList->size);
+	sequenceList->head = (Element*)malloc(sizeof(Element) *sequenceList->size);
 }
 
 int CheckFull(sequence_list *sequenceList){
